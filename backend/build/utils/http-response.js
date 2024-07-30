53dcaa20a4220748fa7b599f96c0458427f8962e
Nagarjuna <chennanagarjuna4@gmail.com>
2024-07-30 12:09:45 +0530

    build folder removed

--- backend/build/utils/http-response.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = HttpResponse;
-function HttpResponse(res, code = undefined, message = undefined, data = undefined, status = 200) {
-    return res
-        .status(status)
-        .json({ status, code, success: status < 400, message, data });
-}
